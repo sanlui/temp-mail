926f8da95db907e7dab9fc78da922ee77d6607a7
sanlui <elbuizx@gmail.com>
2025-08-04 22:38:10 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,11 @@
+// Configurazione JSONBin.io
+const JSONBIN_CONFIG = {
+    BIN_ID: "6891179ff7e7a370d1f3cc16",
+    API_KEY: "$2a$10$lkY6qB9YECqn21lhBBdOC.ItJAkE3F1bX0p.sscP5sV45ET19tcFy",
+    DOMAIN: "tempmaile.xyz"
+};
 
+// Configurazione UI
+const UI_CONFIG = {
+    AUTO_REFRESH_INTERVAL: 30000 // 30 secondi
+};
